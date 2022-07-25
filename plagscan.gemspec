9202c9f0a94468bc6afdf7c8f34d7f8de62992c2
Andrew Bromwich <a.bromwich@gmail.com>
2022-07-25 12:30:39 +1000

    Increase read timeout for Documents#create to 2 minutes
    Allow specifying open/write/read timeout options in request API calls
    Address some code lints
    
    Of late PlagScan calls have been taking more than 60 seconds (default)
    to return a response. Increasing the timeout to 120 seconds *should*
    help

--- plagscan.gemspec
+++ plagscan.gemspec
@@ -31,8 +31,9 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'bundler', '~> 2.0'
   spec.add_development_dependency 'rake', '>= 12.3.3'
   spec.add_development_dependency 'rspec', '~> 3.0'
-  spec.add_development_dependency 'rubocop', '~> 0.74'
-  spec.add_development_dependency 'rubocop-rspec', '~> 1.35'
+  spec.add_development_dependency 'rubocop', '~> 1.28'
+  spec.add_development_dependency 'rubocop-rake', '~> 0.6'
+  spec.add_development_dependency 'rubocop-rspec', '~> 2.10'
   spec.add_development_dependency 'simplecov', '~> 0.17', '< 0.18'
   spec.add_development_dependency 'webmock', '~> 3.0'
 end
--- spec/plagscan/ping_spec.rb
+++ spec/plagscan/ping_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-describe Plagscan do
+describe Plagscan do # rubocop:disable RSpec/FilePath
   describe '.ping' do
     subject(:ping) { described_class.ping }
 
@@ -10,13 +10,15 @@ describe Plagscan do
     let(:failure_result) { Net::HTTPBadRequest.new '1', 400, 'fails' }
 
     it 'returns success for a success result' do
-      expect(Plagscan::Request).to receive(:request).with('ping').and_return success_result
-      expect(ping).to eq true
+      allow(Plagscan::Request).to receive(:request).with('ping').and_return success_result
+      expect(Plagscan::Request).to receive(:request).with('ping')
+      expect(ping).to be true
     end
 
     it 'returns failure for a failure result' do
-      expect(Plagscan::Request).to receive(:request).with('ping').and_return failure_result
-      expect(ping).to eq false
+      allow(Plagscan::Request).to receive(:request).with('ping').and_return failure_result
+      expect(Plagscan::Request).to receive(:request).with('ping')
+      expect(ping).to be false
     end
   end
 end
